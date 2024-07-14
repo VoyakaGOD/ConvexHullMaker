590fcd4f0b3ae43198c59593f11c039edd824d2a
VoyakaGOD <ab3000645@gmail.com>
2024-07-14 16:55:27 +0300

    create MainWindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,43 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QString initialSavePath)
 {
-    ui->setupUi(this);
+    savePath = initialSavePath;
+
+    QPushButton *fileDialogButton = new QPushButton("...");
+    connect(fileDialogButton, &QPushButton::clicked, this, &MainWindow::onFileDialogRequested);
+    pathEditor = new QLineEdit(savePath);
+    QHBoxLayout *pathLayout = new QHBoxLayout();
+    pathLayout->addWidget(pathEditor);
+    pathLayout->addWidget(fileDialogButton);
+
+    QPushButton *saveButton = new QPushButton(QString(QChar(0x2193)));
+    connect(saveButton, &QPushButton::clicked, this, &MainWindow::onSaveRequested);
+    log = new QLabel("Thanks for using!!!");
+    log->setSizePolicy(QSizePolicy::Expanding, log->sizePolicy().verticalPolicy());
+    rewriteOptionCheckBox = new QCheckBox("rewrite");
+    QHBoxLayout *infoLayout = new QHBoxLayout();
+    infoLayout->addWidget(log);
+    infoLayout->addWidget(rewriteOptionCheckBox);
+    infoLayout->addWidget(saveButton);
+
+    auto frame = new QFrame();
+    frame->setMinimumSize(700, 500);
+    frame->setFrameShape(QFrame::Shape::Panel);
+    frame->setFrameShadow(QFrame::Shadow::Raised);
+    frame->setLineWidth(3);
+    auto vlayout = new QVBoxLayout(this);
+    vlayout->addWidget(frame);
+    vlayout->addLayout(pathLayout);
+    vlayout->addLayout(infoLayout);
 }
 
-MainWindow::~MainWindow()
+void MainWindow::onFileDialogRequested()
 {
-    delete ui;
+
+}
+
+void MainWindow::onSaveRequested()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,23 +1,28 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QBoxLayout>
+#include <QCheckBox>
+#include <QLabel>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
+class MainWindow : public QWidget
 {
     Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
 private:
-    Ui::MainWindow *ui;
+    QString savePath;
+    QLineEdit *pathEditor;
+    QLabel *log;
+    QCheckBox *rewriteOptionCheckBox;
+
+public:
+    MainWindow(QString initialSavePath);
+
+private slots:
+    void onFileDialogRequested();
+    void onSaveRequested();
 };
+
 #endif // MAINWINDOW_H
